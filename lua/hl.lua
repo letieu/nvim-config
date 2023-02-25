0d4c17f450914a380a755b9ea49a877847a5ef54
letieu <letieu8@gmail.com>
2023-02-25 22:38:09 +0700

    Change statuscol color

--- lua/hl.lua
+++ lua/hl.lua
@@ -3,3 +3,5 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
