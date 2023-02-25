47f214ed458dadc6bd4426cc1dfa4ba73defb39b
letieu <letieu8@gmail.com>
2023-02-25 19:02:19 +0700

    Make yank blink faster

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,11 +1,11 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
--- ==================== Text yank ====================
+-- blink yanked text
 autocmd('TextYankPost', {
   group = augroup('yank_highlight', {}),
   pattern = '*',
   callback = function()
-    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 300 }
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 150 }
   end,
 })
