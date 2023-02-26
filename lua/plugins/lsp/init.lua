237a314d06f87c528582d6da50d84ecac0fa7b27
letieu <letieu8@gmail.com>
2023-02-26 10:22:54 +0700

    Use bun instead nodejs

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,13 +1,15 @@
 local on_attach = require("plugins.lsp.utils").on_attach
 local capabilities = require("plugins.lsp.utils").capabilities
+local add_bun_prefix = require("plugins.lsp.bun").add_bun_prefix
 
 local lspconfig = require "lspconfig"
+local util = require "lspconfig.util"
 
 local servers = { "html", "cssls", "tsserver", "eslint", "rust_analyzer", "lua_ls", "solidity" }
 
+util.on_setup = util.add_hook_before(util.on_setup, add_bun_prefix)
+
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+  local config = { on_attach = on_attach, capabilities = capabilities }
+  lspconfig[lsp].setup (config)
 end
