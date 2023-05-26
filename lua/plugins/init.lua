74377fc9de5631e1591dded4d88c2cc19014740f
letieu <letieu8@gmail.com>
2023-05-27 01:53:22 +0700

    feat: add friendly-snippets

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,11 @@
 require("lazy").setup({
-  "L3MON4D3/LuaSnip",
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
 
   {
     "neovim/nvim-lspconfig",
