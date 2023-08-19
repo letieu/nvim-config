783257bec1f157cc1fc550c04b54b4562c4d4ca8
letieu <letieu8@gmail.com>
2023-08-19 18:18:12 +0700

    chore: remove bun

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,6 +1,5 @@
 local on_attach = require("plugins.lsp.utils").on_attach
 local capabilities = require("plugins.lsp.utils").capabilities
-local add_bun_prefix = require("plugins.lsp.bun").add_bun_prefix
 
 local lspconfig = require "lspconfig"
 local util = require "lspconfig.util"
@@ -27,8 +26,6 @@ local servers = {
     }
   },
 }
-
-util.on_setup = util.add_hook_before(util.on_setup, add_bun_prefix)
 
 for _, lsp in ipairs(servers) do
   local config = { on_attach = on_attach, capabilities = capabilities }
