598239f73de6ffb277f48358a9994e5e91ec6a16
letieu <letieu8@gmail.com>
2024-01-09 12:06:13 +0700

    refactor my plugins config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,43 +1,23 @@
 return {
   { "neovim/nvim-lspconfig" },
-
   { "nvim-telescope/telescope.nvim",       config = true,                             dependencies = { "nvim-lua/plenary.nvim" } },
-
   { "ThePrimeagen/harpoon",                dependencies = { "nvim-lua/plenary.nvim" } },
-
   { "lukas-reineke/indent-blankline.nvim", config = true,                             main = "ibl", },
-
   { "windwp/nvim-autopairs",               config = true },
-
   { "numToStr/Comment.nvim",               config = true },
-
   { "lewis6991/gitsigns.nvim",             config = true },
-
   { "chentoast/marks.nvim",                config = true },
-
   { "folke/todo-comments.nvim",            config = true },
-
   { 'NvChad/nvim-colorizer.lua',           config = true },
-
   { "j-hui/fidget.nvim",                   config = true },
-
   { "onsails/lspkind.nvim" },
-
   { "kevinhwang91/nvim-hlslens",           config = true },
-
   { "stevearc/dressing.nvim",              config = true },
-
   { "karb94/neoscroll.nvim",               config = true },
-
   { "wintermute-cell/gitignore.nvim" },
-
   { 'mrjones2014/smart-splits.nvim',       config = true },
-
   { "yioneko/nvim-vtsls" },
-
   { 'chrisgrieser/nvim-recorder',          config = true },
-
   { 'akinsho/git-conflict.nvim',           config = true,                             version = "*" },
-
   { "Eandrju/cellular-automaton.nvim" },
 }
