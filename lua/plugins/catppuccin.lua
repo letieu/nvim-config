b6d4e7e22e15a2c69b2cb460b918d8ea9821f293
letieu <letieu8@gmail.com>
2024-03-12 23:07:19 +0700

    dynamic color base on system

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,9 +3,17 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    local flavour = "latte"
+
+    local apple_interface_style = vim.fn.system("defaults read -g AppleInterfaceStyle")
+    if apple_interface_style == "Dark\n" then
+      flavour = "mocha"
+    end
+
     require("catppuccin").setup({
-      flavour = "latte",
+      flavour = flavour,
     })
-    vim.cmd.colorscheme "catppuccin-latte"
+
+    vim.cmd("colorscheme catppuccin-" .. flavour)
   end
 }
