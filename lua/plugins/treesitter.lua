91a695802a32a9f4078153ea2b250b1809a543fc
letieu <letieu8@gmail.com>
2024-03-13 10:15:56 +0700

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,27 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "c",
-      "lua",
-      "gitignore",
-      "gitcommit",
-      "html",
-      "css",
-      "rust",
-      "go",
-      "javascript",
-      "typescript",
-      "markdown",
-    },
-    sync_install = false,
-    auto_install = true,
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    }
-  }
-
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "gitignore",
+        "gitcommit",
+        "html",
+        "css",
+        "rust",
+        "go",
+        "javascript",
+        "typescript",
+        "markdown",
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
 }
