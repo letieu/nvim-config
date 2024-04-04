a1954916dbe9a403191ae882ab781554f0847014
letieu <letieu8@gmail.com>
2024-04-04 22:30:59 +0700

    feat: Update plugins and modify grapple keybindings
    This commit does the following:
    - Updates the commit hashes for several plugins in lazy-lock.json,
      indicating that these plugins have been updated to their latest versions.
    - Changes the keybindings for selecting tags in grapple from control
      keys to semicolon-prefixed keys. This might be due to a personal
      preference or a conflict with other keybindings.

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -10,9 +10,9 @@ return {
     { "<leader>a", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
     { "<leader>h", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
 
-    { "<c-a>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
-    { "<c-s>", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
-    { "<c-q>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
-    { "<c-w>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+    { ";a", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+    { ";s", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+    { ";q", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+    { ";w", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
   },
 }
