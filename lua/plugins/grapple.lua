6bb1834647cb33bc9bf03e5836c71f01625a08f5
letieu <letieu8@gmail.com>
2024-04-05 09:59:50 +0700

    feat(grapple): change scope and keybindings
    The scope of the grapple plugin has been changed from 'git' to
    'git_branch'. This change will allow more specific control over the
    plugin's functionality.
    
    Additionally, the keybindings for selecting tags have been updated.
    Instead of using ';a', ';s', ';q', and ';w', the new keybindings are
    '<c-a>', '<c-s>', '<c-q>', and '<c-w>'. This change will make the
    keybindings more intuitive and easier to use.
    
    A new file 'lazy-lock.json' has also been added to the .gitignore file.
    This will prevent unnecessary tracking of this file in the git history.

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -1,7 +1,7 @@
 return {
   "cbochs/grapple.nvim",
   opts = {
-    scope = "git",
+    scope = "git_branch",
     icons = true,
     status = true,
     quick_select = "asqw",
@@ -10,9 +10,9 @@ return {
     { "<leader>a", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
     { "<leader>h", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
 
-    { ";a", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
-    { ";s", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
-    { ";q", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
-    { ";w", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+    { "<c-a>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+    { "<c-s>", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+    { "<c-q>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+    { "<c-w>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
   },
 }
