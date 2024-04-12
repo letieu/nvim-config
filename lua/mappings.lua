0c0b9d3c21d0ae7ae2ff76e31feac2799ec4456a
letieu <letieu8@gmail.com>
2024-04-12 13:24:12 +0700

    chore: remove old comment

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,23 +12,3 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, nore_and_silent)
 -- ==================== Quickfix ====================
 vim.keymap.set("n", "[q", ":cprev<CR>", nore_and_silent)
 vim.keymap.set("n", "]q", ":cnext<CR>", nore_and_silent)
-
--- vim.keymap.set("n", "<C-h>", function()
---   win_move "h"
--- end, nore_and_silent)
---
--- vim.keymap.set("n", "<C-j>", function()
---   win_move "j"
--- end, nore_and_silent)
---
--- vim.keymap.set("n", "<C-k>", function()
---   win_move "k"
--- end, nore_and_silent)
---
--- vim.keymap.set("n", "<C-l>", function()
---   win_move "l"
--- end, nore_and_silent)
---
--- vim.keymap.set("n", "<C-;>", function()
---   win_move "j"
--- end, nore_and_silent)
