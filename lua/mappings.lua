a4b45571dc10ee001659adb51d8337b6187d0a68
letieu <letieu8@gmail.com>
2024-04-12 09:30:20 +0700

    chore: refactor mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,15 +1,14 @@
-local map = vim.keymap.set
 local nore_and_silent = { noremap = true, silent = true }
 
 -- ==================== General ====================
-map("n", "<ESC>", ":noh<CR>", nore_and_silent)
-map("n", "<leader>w", ":w<CR>", nore_and_silent)
+vim.keymap.set("n", "<ESC>", ":noh<CR>", nore_and_silent)
+vim.keymap.set("n", "<leader>w", ":w<CR>", nore_and_silent)
 
 -- ==================== Diagnostic ====================
-map("n", "<leader>f", vim.diagnostic.open_float, nore_and_silent)
-map("n", "[d", vim.diagnostic.goto_prev, nore_and_silent)
-map("n", "]d", vim.diagnostic.goto_next, nore_and_silent)
+vim.keymap.set("n", "<leader>f", vim.diagnostic.open_float, nore_and_silent)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, nore_and_silent)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, nore_and_silent)
 
 -- ==================== Quickfix ====================
-map("n", "[q", ":cprev<CR>", nore_and_silent) -- previous quickfix
-map("n", "]q", ":cnext<CR>", nore_and_silent) -- next quickfix
+vim.keymap.set("n", "[q", ":cprev<CR>", nore_and_silent) -- previous quickfix
+vim.keymap.set("n", "]q", ":cnext<CR>", nore_and_silent) -- next quickfix
