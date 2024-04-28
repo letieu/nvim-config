a6a35e084dbabb69c23c57f2454113d625103742
letieu <letieu8@gmail.com>
2024-04-28 16:29:37 +0700

    feat: add open quickfix key

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,6 +12,7 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, nore_and_silent)
 -- ==================== Quickfix ====================
 vim.keymap.set("n", "[q", ":cprev<CR>", nore_and_silent)
 vim.keymap.set("n", "]q", ":cnext<CR>", nore_and_silent)
+vim.keymap.set("n", "<leader>q", ":copen<CR>", nore_and_silent)
 
 -- ==================== Mark ====================
 vim.keymap.set("n", "<leader>mc", ":delmarks a-z<CR>", nore_and_silent)
