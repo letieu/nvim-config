98edb0b7c5fbb98a2a34a32d0360b4c6f69c3ba4
letieu <letieu8@gmail.com>
2024-05-03 05:26:23 +0700

    feat: telescope -> fzf-lua

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,44 @@
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("fzf-lua").setup {}
+  end,
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("fzf-lua").files()
+      end,
+      desc = "Find files",
+    },
+    {
+      "<leader>fw",
+      function()
+        require("fzf-lua").live_grep()
+      end,
+      desc = "Fzf live grep",
+    },
+    {
+      "<leader>fd",
+      function()
+        require("fzf-lua").diagnostics_document()
+      end,
+      desc = "Fzf diagnostics",
+    },
+    {
+      "<leader>fD",
+      function()
+        require("fzf-lua").diagnostics_workspace()
+      end,
+      desc = "Fzf workspace diagnostics",
+    },
+    {
+      "<leader>gs",
+      function()
+        require("fzf-lua").git_status()
+      end,
+      desc = "Fzf git status",
+    },
+  },
+}
