91709c4c65dd16d596c85694e9f5482792a550d0
letieu <letieu8@gmail.com>
2024-06-03 21:12:43 +0700

    feat: use nvim terminal instead of wezterm

--- init.lua
+++ init.lua
@@ -16,3 +16,4 @@ require("lazy").setup "plugins"
 
 require "autocmd"
 require "mappings"
+require "terminal"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,9 @@
 -- This file for some plugins that don't need custom configuration, just install and use it with 1 line of code
 -- For plugins that need custom configuration, create a new file in lua/plugins/
 return {
-  { "folke/neodev.nvim", opts = {}, ft = { "lua" } },
-  { "windwp/nvim-autopairs", config = true, event = { "InsertEnter" } },
-  { "j-hui/fidget.nvim", config = true, event = { "VeryLazy" } },
+  { "folke/neodev.nvim",        opts = {},              ft = { "lua" } },
+  { "windwp/nvim-autopairs",    config = true,          event = { "InsertEnter" } },
+  { "j-hui/fidget.nvim",        config = true,          event = { "VeryLazy" } },
   { "folke/todo-comments.nvim", event = { "VeryLazy" }, config = true },
+  { "williamboman/mason.nvim",  config = true }
 }
