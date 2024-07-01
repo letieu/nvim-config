b836a32306e2bce29fa464e2b84a9e4b05f5e5a7
letieu <letieu8@gmail.com>
2024-07-01 21:27:50 +0700

    chore: simplify copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,25 +5,11 @@ return {
   config = function()
     require('copilot').setup({
       panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<M-CR>"
-        },
-        layout = {
-          position = "bottom", -- | top | left | right
-          ratio = 0.4
-        },
+        enabled = false,
       },
       suggestion = {
         enabled = true,
         auto_trigger = true,
-        hide_during_completion = true,
-        debounce = 75,
         keymap = {
           accept = "<M-l>",
           accept_word = false,
@@ -32,17 +18,6 @@ return {
           prev = "<M-[>",
           dismiss = "<C-]>",
         },
-      },
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
       },
       copilot_node_command = 'node', -- Node.js version must be > 18.x
       server_opts_overrides = {},
