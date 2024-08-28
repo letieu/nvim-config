b4ca18564ed9aeb9a150ab91903e490f1059a2bf
letieu <letieu8@gmail.com>
2024-08-28 12:24:43 +0700

    chore: update conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,9 +4,9 @@ return {
     require("conform").setup {
       formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { { "prettier", "eslint_d" } },
-        typescript = { { "prettier", "eslint_d" } },
-        vue = { { "prettier", "eslint_d" } },
+        javascript = { "prettier", "eslint_d", stop_after_first = true },
+        typescript = { "prettier", "eslint_d", stop_after_first = true },
+        vue = { "prettier", "eslint_d" },
         python = { "isort", "black" },
         json = { "jq" },
         yaml = { "yq" },
