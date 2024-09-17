221596a4fc4d70f3d19c3fbd64718bcac29f3763
letieu <letieu8@gmail.com>
2024-09-17 11:27:15 +0700

    chore: add html, css formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,8 @@ return {
   config = function()
     require("conform").setup {
       formatters_by_ft = {
+        html = { "prettier" },
+        css = { "prettier" },
         lua = { "stylua" },
         javascript = { "prettier", "eslint_d", stop_after_first = true },
         typescript = { "prettier", "eslint_d", stop_after_first = true },
