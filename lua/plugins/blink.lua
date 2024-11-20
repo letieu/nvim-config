365899bfe9bcf10059f9a5c258cb7f92e942918c
letieu <letieu8@gmail.com>
2024-11-20 10:07:33 +0700

    feat: add complete for vim dadbod

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,8 +3,16 @@ return {
   lazy = false,
   version = 'v0.*',
   opts = {
+    sources = {
+      completion = {
+        enabled_providers = { "lsp", "path", "snippets", "buffer", "dadbod" },
+      },
+      providers = {
+        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+      },
+    },
     keymap = {
-      ['<Tab>'] = { 'select_and_accept' },
+      ['<Tab>'] = { 'select_and_accept', 'fallback' },
       ['<C-j>'] = { 'select_next' },
       ['<C-k>'] = { 'select_prev' },
     },
