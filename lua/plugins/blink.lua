274a7ca710be4973c2b96c3ba78a41a5c879f89f
letieu <letieu8@gmail.com>
2024-12-24 09:55:28 +0700

    chore: bump blink version

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,17 +4,16 @@ return {
   version = 'v0.*',
   opts = {
     sources = {
-      completion = {
-        enabled_providers = { "lsp", "path", "snippets", "buffer", "dadbod" },
-      },
       providers = {
         dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
       },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'dadbod' },
     },
     keymap = {
       ['<Tab>'] = { 'select_and_accept', 'fallback' },
       ['<C-j>'] = { 'select_next' },
       ['<C-k>'] = { 'select_prev' },
     },
+    signature = { enabled = true }
   }
 }
