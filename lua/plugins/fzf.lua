5b1f7ed3dbd9f70a2dc5070b0dfd82d787acde8c
letieu <letieu8@gmail.com>
2025-01-11 00:05:00 +0700

    refactor: fzf config to one file

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,11 +1,27 @@
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local config = require("helper/fzf-config")
-    require("fzf-lua").setup(config)
+  opts = {
+    desc     = "fzf-lua defaults with `sk` as binary",
+    fzf_bin  = "sk",
+    fzf_opts = { ["--no-separator"] = false },
+    winopts  = {
+      preview = {
+        hidden = 'hidden', -- hidden|nohidden
+      },
+    },
+    lsp      = {
+      code_actions = {
+        previewer = false,
+        winopts = { height = 0.5, width = 0.5 },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("fzf-lua").setup(opts)
     require("fzf-lua").register_ui_select()
   end,
+  event = { "LspAttach" },
   keys = {
     {
       "<leader>ff",
