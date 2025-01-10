f088925dbe2502f87100fb31294d817e3a7fd568
letieu <letieu8@gmail.com>
2025-01-10 22:58:28 +0700

    feat: remove some plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,17 +1,8 @@
 -- This file for some plugins that don't need custom configuration, just install and use it with 1 line of code
 -- For plugins that need custom configuration, create a new file in lua/plugins/
 return {
-  { "folke/neodev.nvim",         ft = { "lua" } },
   { "windwp/nvim-autopairs",     config = true,          event = { "InsertEnter" } },
-  { "j-hui/fidget.nvim",         config = true,          event = { "VeryLazy" } },
-  { "folke/todo-comments.nvim",  event = { "VeryLazy" }, config = true },
   { "williamboman/mason.nvim",   event = { "VeryLazy" }, config = true },
   { 'akinsho/git-conflict.nvim', config = true },
   { "xzbdmw/colorful-menu.nvim", config = true,          event = { "InsertEnter" } },
-  {
-    "karb94/neoscroll.nvim",
-    opts = {
-      easing = "sine"
-    }
-  }
 }
