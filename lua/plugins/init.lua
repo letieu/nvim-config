605491d46907e7cc671e16f60a0b4d86e08e6080
letieu <letieu8@gmail.com>
2025-02-17 11:15:23 +0700

    feat: add colorizer

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,9 @@
 -- This file for some plugins that don't need custom configuration, just install and use it with 1 line of code
 -- For plugins that need custom configuration, create a new file in lua/plugins/
 return {
-  { "windwp/nvim-autopairs",     config = true, event = { "InsertEnter" } },
-  { "williamboman/mason.nvim",   config = true, event = { "VeryLazy" } },
-  { 'akinsho/git-conflict.nvim', config = true },
-  { "xzbdmw/colorful-menu.nvim", config = true, event = { "InsertEnter" } },
+  { "windwp/nvim-autopairs",       config = true, event = { "InsertEnter" } },
+  { "williamboman/mason.nvim",     config = true, event = { "VeryLazy" } },
+  { 'akinsho/git-conflict.nvim',   config = true },
+  { "xzbdmw/colorful-menu.nvim",   config = true, event = { "InsertEnter" } },
+  { "norcalli/nvim-colorizer.lua", config = true }
 }
