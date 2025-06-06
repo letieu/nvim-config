ca789a9d5b3f79e007aa88bbc50b712e0e2782e9
letieu <letieu8@gmail.com>
2025-06-06 22:26:27 +0700

    feat: config formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,9 +5,18 @@ return {
       formatters_by_ft = {
         html = { "prettier" },
         css = { "prettier" },
+        lua = { "stylua" },
+        javascript = { "prettier", "eslint_d", stop_after_first = true },
+        typescript = { "prettier", "eslint_d", stop_after_first = true },
+        typescriptreact = { "prettier", "eslint_d", stop_after_first = true },
+        vue = { "prettier", "eslint_d" },
+        python = { "isort", "black" },
         json = { "jq" },
         yaml = { "yq" },
+        dart = { "dartfmt" },
         sql = { "sqlfmt" },
+        go = { "gofmt", "goimports" },
+        nix = { "nixpkgs-fmt" },
       },
     }
   end,
