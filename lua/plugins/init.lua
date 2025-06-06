a5cc8339f36423cb8b1c3693f34a09463e250fad
letieu <letieu8@gmail.com>
2025-06-06 22:28:18 +0700

    feat: use tiny inline dianogstic for bester ui

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,4 +9,13 @@ return {
     dependencies = { "MunifTanjim/nui.nvim" },
     opts = {},
   },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000,    -- needs to be loaded in first
+    config = function()
+      require('tiny-inline-diagnostic').setup()
+      vim.diagnostic.config({ virtual_text = false })   -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end
+  }
 }
