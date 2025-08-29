4e39b498764c2533070ab306b0370dbf5b03220f
letieu <letieu8@gmail.com>
2025-08-30 00:26:31 +0700

    refactor(plugins): refactor plugin list for better readability

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,37 +1,13 @@
 -- This file for some plugins that don't need custom configuration, just install and use it with 1 line of code
 -- For plugins that need custom configuration, create a new file in lua/plugins/
 return {
-  { "windwp/nvim-autopairs",     config = true, event = { "InsertEnter" } },
-  { 'akinsho/git-conflict.nvim', config = true },
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
-  },
-  {
-    "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy", -- Or `LspAttach`
-    priority = 1000,    -- needs to be loaded in first
-    config = function()
-      require('tiny-inline-diagnostic').setup()
-      vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
-    end
-  },
+  { "windwp/nvim-autopairs",       config = true,              event = { "InsertEnter" } },
+  { 'akinsho/git-conflict.nvim',   config = true },
+  { "mason-org/mason.nvim",        opts = {} },
+  { "norcalli/nvim-colorizer.lua", cmd = { "ColorizerToggle" } },
   {
     "mistweaverco/kulala.nvim",
     ft = { "http", "rest" },
-    opts = {
-      -- your configuration comes here
-      global_keymaps = true,
-    },
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = {}
-  },
-  {
-    "norcalli/nvim-colorizer.lua",
-    cmd = { "ColorizerToggle" }
+    opts = { global_keymaps = true },
   }
 }
