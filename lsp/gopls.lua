163ac9a3ed073c3ada063dd3a981bf1e845b1fbd
letieu <letieu8@gmail.com>
2026-04-21 12:38:55 +0700

    remove all

--- lsp/gopls.lua
+++
@@ -1,5 +0,0 @@
-return {
-  cmd = { 'gopls' },
-  filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-  root_markers = { 'go.mod', 'go.work', '.git' },
-}
--- lsp/templ.lua
+++
@@ -1,5 +0,0 @@
-return {
-  cmd = { 'templ', 'lsp' },
-  filetypes = { 'templ' },
-  root_markers = { 'go.work', 'go.mod', '.git' },
-}
