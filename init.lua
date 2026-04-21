90c77380026c7bfddab1e64a88c9b375e052242d
letieu <letieu8@gmail.com>
2026-04-21 12:39:16 +0700

    init native config

---
+++ init.lua
@@ -0,0 +1,93 @@
+vim.g.mapleader = " "
+vim.opt.shortmess:append("c") -- Prevents showing extra messages when using completion
+vim.opt.cmdheight = 1
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.timeoutlen = 500
+vim.opt.swapfile = false
+vim.opt.updatetime = 4000 -- Time in milliseconds of inactivity before calling CursorHold or writing to swap
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+
+local my_group_id = vim.api.nvim_create_augroup('my.config', {
+  clear = false
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = my_group_id,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank { higroup = "IncSearch", timeout = 150 }
+  end,
+})
+
+-- Ensures the menu appears even for a single match and uses the native popup window.
+vim.opt.completeopt = "menu,menuone,noselect,popup"
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp_completion", { clear = true }),
+  callback = function(args)
+    local client_id = args.data.client_id
+    if not client_id then
+      return
+    end
+
+    local client = vim.lsp.get_client_by_id(client_id)
+    if client and client:supports_method("textDocument/completion") then
+      -- Enable native LSP completion for this client + buffer
+      vim.lsp.completion.enable(true, client_id, args.buf, {
+        autotrigger = true,
+      })
+
+      vim.bo.autocomplete = true
+    end
+  end,
+})
+
+vim.lsp.enable({ 'gopls', 'vtsls', 'eslint', 'lua_ls', 'templ', 'zls', 'dartls', 'vue_ls', 'html', 'rust_analyzer', 'clangd' })
+vim.keymap.set('n', '<leader>f', vim.diagnostic.open_float)
+
+require('vim._core.ui2').enable({})
+
+-- oil
+vim.pack.add({
+  'https://github.com/stevearc/oil.nvim'
+})
+require("oil").setup()
+vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open oil, open parent" })
+
+-- fff
+vim.pack.add({ 'https://github.com/dmtrKovalenko/fff.nvim' })
+-- the plugin will automatically lazy load
+vim.g.fff = {
+  lazy_sync = true, -- start syncing only when the picker is open
+  debug = {
+    enabled = true,
+    show_scores = true,
+  },
+}
+vim.keymap.set(
+  'n',
+  '<leader>ff',
+  function() require('fff').find_files() end,
+  { desc = 'FFFind files' }
+)
+vim.keymap.set(
+  'n',
+  '<leader>fw',
+  function() require('fff').live_grep() end,
+  { desc = 'Live grep' }
+)
+
+-- theme
+vim.pack.add({
+  "https://github.com/rose-pine/neovim"
+})
+vim.cmd("colorscheme rose-pine")
---
+++ lsp/vue_ls.lua
@@ -0,0 +1,34 @@
+return {
+  cmd = { 'vue-language-server', '--stdio' },
+  filetypes = { 'vue' },
+  root_markers = { 'package.json' },
+  on_init = function(client)
+    client.handlers['tsserver/request'] = function(_, result, context)
+      local vtsls_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+      local clients = {}
+
+      vim.list_extend(clients, vtsls_clients)
+
+      if #clients == 0 then
+        vim.notify('Could not find `ts_ls` or `vtsls` lsp client, required by `vue_ls`.', vim.log.levels.ERROR)
+        return
+      end
+      local ts_client = clients[1]
+
+      local param = unpack(result)
+      local id, command, payload = unpack(param)
+      ts_client:exec_cmd({
+        title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+        command = 'typescript.tsserverRequest',
+        arguments = {
+          command,
+          payload,
+        },
+      }, { bufnr = context.bufnr }, function(_, r)
+        local response_data = { { id, r and r.body } }
+        ---@diagnostic disable-next-line: param-type-mismatch
+        client:notify('tsserver/response', response_data)
+      end)
+    end
+  end,
+}
