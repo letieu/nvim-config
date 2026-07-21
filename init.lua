a9e34bcc6ffc0cc380d976d2f7d7fba67c1022fc
tieu-le-tpv-clv <tieu.le.tpv@one-line.com>
2026-07-21 10:54:48 +0700

    add treesitter

--- init.lua
+++ init.lua
@@ -152,3 +152,9 @@ vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
 vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
 vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
 vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+
+vim.pack.add({"https://github.com/nvim-treesitter/nvim-treesitter"})
+
+require('nvim-treesitter').setup {
+  install_dir = vim.fn.stdpath('data') .. '/site'
+}
